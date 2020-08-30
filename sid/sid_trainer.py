7431aa6dc6c48117147f7a36a39f1174ad5b1dc1
Shouta Ukai <shouta.ukai@gmail.com>
2020-08-31 00:01:33 +0900

    Adapted to the redifined data format

--- sid/sid_trainer.py
+++ sid/sid_trainer.py
@@ -4,18 +4,19 @@ Created on 2020/07/16
 @author: ukai
 '''
 
+from builtins import isinstance
+
 import torch
 from torch.optim import Adam
 
-from pole_batch_data_agent import PoleBatchDataAgent
-from pole_batch_data_environment import PoleBatchDataEnvironment
+from sid_agent import SidAgent
+from sid_batch_data_agent import SidBatchDataAgent
+from sid_batch_data_environment import SidBatchDataEnvironment
+from sid_environment import SidEnvironment
 from sl_trainer import SlTrainer
-from builtins import isinstance
-from pole_agent import PoleAgent
-from pole_environment import PoleEnvironment
 
 
-class PoleTrainer(SlTrainer):
+class SidTrainer(SlTrainer):
     '''
     classdocs
     '''
@@ -24,18 +25,18 @@ class PoleTrainer(SlTrainer):
     def __init__(self, agent, environment):
         SlTrainer.__init__(self, agent, environment)
         
-        assert isinstance(agent, PoleAgent)
-        assert isinstance(environment, PoleEnvironment)
+        assert isinstance(agent, SidAgent)
+        assert isinstance(environment, SidEnvironment)
         
         self.optimizer = Adam(agent.parameters())
         
         
     def update(self, batchDataIn, batchDataOut):
         
-        assert isinstance(batchDataIn, PoleBatchDataEnvironment)
-        assert isinstance(batchDataOut, PoleBatchDataAgent)
+        assert isinstance(batchDataIn, SidBatchDataEnvironment)
+        assert isinstance(batchDataOut, SidBatchDataAgent)
         
-        _Y = batchDataIn._Y # (Nhrz+1, *, Ny)
+        _Y = batchDataIn._Y2 # (Nhrz+1, *, Ny)
         _Yhat = batchDataOut._Yhat # (Nhrz+1, * , Ny)
         
         _loss = torch.mean((_Y - _Yhat)**2)
