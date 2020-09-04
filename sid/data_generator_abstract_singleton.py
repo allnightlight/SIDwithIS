3b7c2224934387ce47e5823b2f0bfc437a8176df
Shouta Ukai <shouta.ukai@gmail.com>
2020-09-04 15:34:53 +0900

    Added the test data in abstract data generator

--- sid/data_generator_abstract_singleton.py
+++ sid/data_generator_abstract_singleton.py
@@ -27,3 +27,19 @@ class DataGeneratorAbstractSingleton(object):
             cls._instances[key] = _inst
         return _inst
         
+        
+    def __init__(self, Nsample, Ny, Nu):
+        
+        self.Nsample = Nsample
+        self.Ny = Ny
+        self.Nu = Nu
+        
+        rstate = np.random.RandomState(seed=0)
+        
+        self.Y = rstate.randn(Nsample, Ny) # (Nsample, Ny)
+        self.U = rstate.randn(Nsample, Nu) # (Nsample, Nu)
+        self.Ev = rstate.randint(2, size=(Nsample,)) # (Nsample,)
+        self.IsNaN = rstate.rand(Nsample) < 1/2**4 # (Nsample,)
+        
+
+        
