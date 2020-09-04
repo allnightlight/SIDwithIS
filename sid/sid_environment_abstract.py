9ea1aa9fdd4d7ea84ee094d9f19f601e5147d918
Shouta Ukai <shouta.ukai@gmail.com>
2020-09-04 17:04:26 +0900

    Refactor: implemented shared function to find available indexes

--- sid/sid_environment_abstract.py
+++ sid/sid_environment_abstract.py
@@ -28,66 +28,68 @@ class SidEnvironmentAbstract(SlEnvironment):
         
         self.dataGeneratorSingleton = dataGeneratorSingleton
         assert Ntrain < dataGeneratorSingleton.Nsample
+        assert Ntrain > N0 + N1
+        self.Nsample = dataGeneratorSingleton.Nsample
         self.Ntrain = Ntrain
         self.Ntest = dataGeneratorSingleton.Nsample-Ntrain
+        assert self.Ntest > N0 + N1
         self.Nbatch = Nbatch
         self.N0 = N0 # estimation horizon's length
         self.N1 = N1 # prediction horizon's length
         self.sampling_balance = sampling_balance # the proportional rate of samples with ev == 1 in a batch data
         
-    def generateBatchDataIterator(self):
-
-
-        Nbatch = self.Nbatch 
-        N0 = self.N0
-        N1 = self.N1 
+    # <<protected>>
+    def getAvailableIndex(self, segment):
         
-        for _ in range((self.Ntrain-N0-N1)//Nbatch):
-            idx = np.random.randint(low=0, high=self.Ntrain-N0-N1, size=(Nbatch,))
-            idx = idx.reshape((1,-1)) + np.arange(N0+N1).reshape(-1,1) # (N0+N1, Nbatch)
-            U0batch = self.dataGeneratorSingleton.U[idx[:N0,:],:] # (N0, *, Nu)
-            U1batch = self.dataGeneratorSingleton.U[idx[N0:,:],:] # (N1, *, Nu)
-            
-            Y0batch = self.dataGeneratorSingleton.Y[idx[:N0,:],:] # (N0, *, Ny)
-            Y2batch = self.dataGeneratorSingleton.Y[idx[(N0-1):,:],:] # (N2, *, Ny)
-            
-            Ev0batch = self.dataGeneratorSingleton.Ev[idx[:N0,:]] # (N0, *)
-            Ev1batch = self.dataGeneratorSingleton.Ev[idx[N0:,:]] # (N1, *)
-            
-            _U0 = torch.tensor(U0batch.astype(np.float32))
-            _U1 = torch.tensor(U1batch.astype(np.float32))
-            _Ev0 = torch.tensor(Ev0batch.astype(np.float32))
-            _Ev1 = torch.tensor(Ev1batch.astype(np.float32))
-            _Y0 = torch.tensor(Y0batch.astype(np.float32))
-            _Y2 = torch.tensor(Y2batch.astype(np.float32))
-            
-            batchDataEnvironment = SidBatchDataEnvironment( _U0, _Ev0, _Y0, _U1, _Ev1, _Y2)
-            
-            yield batchDataEnvironment
-    
-    def getTestBatchData(self):
+        idx = None
+        if segment == "train":
+            idx =np.arange(self.N0, self.Ntrain-self.N1) # (* = Ntrain-N0-N1)
+        
+        if segment == "test":
+            idx =np.arange(self.Ntrain+self.N0, self.Nsample-self.N0-self.N1) # (* = Ntest-N0-N1)
+        
+        idx = idx.reshape((1,-1)) + np.arange(-self.N0, self.N1).reshape((-1, 1)) # (N0+N1, *)
+        idxAvailable = idx[:, ~np.any(self.dataGeneratorSingleton.IsNaN[idx], axis=0)] # (N0+N1, Navailable), * = Navailable + #NaN
+        
+        return idxAvailable # (N0+N1, Navailable)
+        
+    # <<private>>
+    def extractBatchData(self, idx):
+        # idx: (N0+N1, *)
         
         N0 = self.N0
-        N1 = self.N1 
         
-        idx = np.arange(self.Ntrain+N0, self.Ntrain+self.Ntest-N1) # (*, ), where Nbatch = Ntest - N0 - N1.
-        idx = idx.reshape((1,-1)) + np.arange(N0+N1).reshape(-1,1) # (N0+N1, *)
         U0batch = self.dataGeneratorSingleton.U[idx[:N0,:],:] # (N0, *, Nu)
         U1batch = self.dataGeneratorSingleton.U[idx[N0:,:],:] # (N1, *, Nu)
         
         Y0batch = self.dataGeneratorSingleton.Y[idx[:N0,:],:] # (N0, *, Ny)
         Y2batch = self.dataGeneratorSingleton.Y[idx[(N0-1):,:],:] # (N2, *, Ny)
-
+        
         Ev0batch = self.dataGeneratorSingleton.Ev[idx[:N0,:]] # (N0, *)
         Ev1batch = self.dataGeneratorSingleton.Ev[idx[N0:,:]] # (N1, *)
-
+        
         _U0 = torch.tensor(U0batch.astype(np.float32))
         _U1 = torch.tensor(U1batch.astype(np.float32))
         _Ev0 = torch.tensor(Ev0batch.astype(np.float32))
         _Ev1 = torch.tensor(Ev1batch.astype(np.float32))
         _Y0 = torch.tensor(Y0batch.astype(np.float32))
         _Y2 = torch.tensor(Y2batch.astype(np.float32))
-                
+        
         batchDataEnvironment = SidBatchDataEnvironment( _U0, _Ev0, _Y0, _U1, _Ev1, _Y2)
         
         return batchDataEnvironment
+
+    def generateBatchDataIterator(self):
+
+        Nbatch = self.Nbatch
+        idxAvailable = self.getAvailableIndex("train") # (N0+N1, Navailable) 
+        Navailable = idxAvailable.shape[1]
+        
+        for _ in range(Navailable//Nbatch):
+            idx = idxAvailable[:, np.random.randint(low=0, high=Navailable, size=(Nbatch,))] # (N0+N1, *)
+            yield self.extractBatchData(idx)
+    
+    def getTestBatchData(self):
+        
+        idxAvailable = self.getAvailableIndex("test") # (N0+N1, Navailable) 
+        return self.extractBatchData(idxAvailable)
